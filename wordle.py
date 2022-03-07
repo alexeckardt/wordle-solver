9bd1aa40a62bf0a565e8a36f73d154a0fc60ae77
Alex Eckardt <realalexeck@gmail.com>
2022-03-07 15:52:12 -0500

    One File; Double Click Fix

--- wordle.py
+++ wordle.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 
 import os
-import probability
 
 def solve_wordle():
 
@@ -11,21 +11,19 @@ def solve_wordle():
 
     for turn in range(6):
 
-        print("-------------------------------\nTurn {} Guess:".format(turn + 1))
-        guess = input().upper();
+        print("-------------------------------\nTurn {}".format(turn + 1))
+        guess = input("Enter Word Guess: ").upper();
     
-        print("Known Order (use ; for unknown order): ")
-        knownOrder = input();
+        knownOrder = input("Known Order (use ; for unknown order): ");
 
         if ';' not in knownOrder:
             print("Congradulations!");
             break
 
-        print("Known Letters (not order): ")
-        unknownPosition = input();
+        unknownPosition = input("Known Letters (not order): ");
 
         knownLetters = set(unknownPosition.upper() + knownOrder.replace(";", "").upper());
-        print(knownLetters);
+        #print(knownLetters);
 
 
         for letter in guess:
@@ -43,8 +41,8 @@ def solve_wordle():
                             badGoodLetterPoses[pos].add(letter)
 
 
-        print("no letters in this pos: ", badGoodLetterPoses);
-        print("letters cannot use: ", blacklist);
+        #print("no letters in this pos: ", badGoodLetterPoses);
+        #print("letters cannot use: ", blacklist);
 
         #
         printList = []
@@ -124,7 +122,7 @@ def print_return_result(listOfValidWords):
 
     #Get Vals
     for word in listOfValidWords:
-        org[word] = probability.assign_goodness_value(word)
+        org[word] = assign_goodness_value(word)
 
     #Org
     sortorg = sorted(org.items(), key=lambda x: x[1], reverse=True)
@@ -134,6 +132,58 @@ def print_return_result(listOfValidWords):
         print(sortorg[i][0])
     
 
+def get_letter_probs():
 
+    return {'A': 10.18, 'B': 2.44, 'C': 3.77, 'K': 1.71, 'F': 1.69,
+            'T': 5.59, 'S': 7.14, 'E': 10.28, 'H': 2.91, 'Y': 2.65,
+            'O': 6.73, 'D': 3.55, 'L': 5.65, 'M': 3.34, 'R': 6.95,
+            'I': 6.46, 'U': 4.4, 'V': 1.37, 'N': 4.98, 'Z': 0.43,
+            'G': 2.49, 'P': 2.94, 'X': 0.46, 'J': 0.41, 'W': 1.23, 'Q': 0.24}
     
+
+def generate_letter_probs():
+    
+    words = five_letter_words();
+
+    count = {}
+    totalLetters = 0
+    for word in words:
+        for letter in word:
+            count[letter] = count.get(letter, 0) + 1
+            totalLetters += 1
+
+    for letter in count:
+        count[letter] = round(count[letter] / totalLetters * 10000) / 100
+
+    return count
+
+def assign_goodness_value(word):
+
+    #Bad
+    if len(word) != 5:
+        return 0
+
+    #Grab
+    letterProb = get_letter_probs();
+    letterCount = {}
+
+    #Get Sum
+    totalSum = 0
+    highestLetterCount = 1
+    for letter in word:
+        totalSum += letterProb[letter];
+        letterCount[letter] = letterCount.get(letter, 0) + 1
+
+        if letterCount[letter] > highestLetterCount:
+            highestLetterCount = letterCount[letter]
+
+    #Compile
+    value = (totalSum * 1.4) / (highestLetterCount**1.5)
+    #print("{}: {}".format(word, value))
+    return value
+
+#Run
+if (__name__ == "__main__"):
+    solve_wordle();    
+
         
